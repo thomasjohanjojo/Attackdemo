fa919116bf1016f00ea9f29bcfba1cb4cd4d6a9f
Thomas Johan Jojo <thomasjohanjojo@gmail.com>
2021-04-28 18:44:21 +0530

    Facing Direction detection script added and collisions with enemy has been successfully detected

--- Attackdemo/Assets/Scripts/Attack.cs
+++ Attackdemo/Assets/Scripts/Attack.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+
+    public BoxCollider2D myAtackBoxCollider;
+    public float playerFacingDirection;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,30 @@ public class Attack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckPlayerFacingDirection();
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            Debug.Log("Collision with enemy successfully detected");
+        }
+    }
+
+    void CheckPlayerFacingDirection()
+    {
+        if (gameObject.transform.rotation.y == 0)
+        {
+            playerFacingDirection = 1; // 1 means right
+
+        }
+        else if (gameObject.transform.rotation.y == -1)
+        {
+            playerFacingDirection = -1;
+
+        }
+    }
+
+
 }
