b212ba3ec93b590117a6292a8f15c44f6d202578
Thomas Johan Jojo <thomasjohanjojo@gmail.com>
2021-04-29 19:36:43 +0530

    Attack script, added without much collissions

--- Attackdemo/Assets/Scripts/Attack.cs
+++ Attackdemo/Assets/Scripts/Attack.cs
@@ -4,15 +4,86 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+    public float pushBackForceOfFirstAttack;
+
+    public bool isAttackButtonPressed;
+
+    public bool DoPushAttackBooleanForTheWholeScript = true;
+
+
+    public BoxCollider2D myAtackBoxCollider;
+    public float playerFacingDirection;
+    public Rigidbody2D enemyRigidBody;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (DoPushAttackBooleanForTheWholeScript)
+        {
+            CheckPlayerFacingDirection();
+            CheckIfAttackButtonIsPressedSinceInputCanOnlyBeTakenThroughUpdateMethod();
+            IfAttackButtonIsPressedAndEnemyHasBeenDetectedThenPushTheEnemy();
+        }
+
+    }
+
+    void IfAttackButtonIsPressedAndEnemyHasBeenDetectedThenPushTheEnemy()
+    {
+        if (enemyRigidBody)
+        {
+            Debug.Log("Enemy rigidbody attached");
+
+            if (isAttackButtonPressed == true)
+            {
+                Vector2 pushBackForceToAddAsVector = new Vector2(playerFacingDirection * pushBackForceOfFirstAttack, 0f);
+                enemyRigidBody.AddForce(pushBackForceToAddAsVector, ForceMode2D.Impulse);
+                Debug.Log("pushed");
+                enemyRigidBody = null; // to free up the rigidbody reference
+            }
+        }
+    }
+
+    void CheckIfAttackButtonIsPressedSinceInputCanOnlyBeTakenThroughUpdateMethod()
+    {
+        if (Input.GetAxisRaw("Fire1") > 0)
+        {
+            isAttackButtonPressed = true;
+        }
+        else
+        {
+            isAttackButtonPressed = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            Debug.Log("Collision with enemy successfully detected");
+
+            enemyRigidBody = collision.gameObject.GetComponent<Rigidbody2D>();
+
+        }
+    }
+
+    void CheckPlayerFacingDirection()
+    {
+        if (gameObject.transform.rotation.y == 0)
+        {
+            playerFacingDirection = 1; // 1 means right
+
+        }
+        else if (gameObject.transform.rotation.y == -1)
+        {
+            playerFacingDirection = -1;
+
+        }
     }
 }
