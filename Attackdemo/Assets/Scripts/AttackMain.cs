bfb56e5a3c742a9355f353b59a6d4d0946723716
Thomas Johan Jojo <thomasjohanjojo@gmail.com>
2021-05-08 15:07:54 +0530

    Removed the extra script and added its capability to the main script

--- Attackdemo/Assets/Scripts/AttackMain.cs
+++ Attackdemo/Assets/Scripts/AttackMain.cs
@@ -8,8 +8,29 @@ public class AttackMain : MonoBehaviour
     public playerController2D playerControllerReference;
 
 
+
+
+    public float pushBackForceOfFirstAttack;
+    public bool doThePush;
+    public BoxCollider2D myAtackBoxCollider;
+    public float playerFacingDirection;
+    public Rigidbody2D enemyRigidBody;
+
+    public bool isGrounded;
+    [SerializeField]
+    Transform groundCheck;
+    [SerializeField]
+    Transform groundCheckR;
+    [SerializeField]
+    Transform groundCheckL;
+
+
+
+
+
+
+
     public int attackIDCounterWhichIsUsedToControlWhichAttackIsToBeExecuted;
-    public Attack pushBackScript;
     public int damageOfFirstAttack;
     public float windingUpTimeOfFirstAttack;
     public int damageOfSecondAttack;
@@ -28,6 +49,10 @@ public class AttackMain : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CheckIfPlayerIsGrounded();
+
+        CheckPlayerFacingDirection();
+        IfDoThePushBooleanIsActivatedAndEnemyHasBeenDetectedThenPushTheEnemy();
 
         if (canAttack)
         {
@@ -35,12 +60,93 @@ public class AttackMain : MonoBehaviour
         }
     }
 
+
+    public void CheckIfPlayerIsGrounded()
+    {
+        if ((Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"))) ||
+                 (Physics2D.Linecast(transform.position, groundCheckR.position, 1 << LayerMask.NameToLayer("Ground"))) ||
+                  (Physics2D.Linecast(transform.position, groundCheckL.position, 1 << LayerMask.NameToLayer("Ground"))))
+        {
+            isGrounded = true;
+
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    void IfDoThePushBooleanIsActivatedAndEnemyHasBeenDetectedThenPushTheEnemy()
+    {
+        if (enemyRigidBody)
+        {
+            Debug.Log("Enemy rigidbody attached");
+
+            if (doThePush == true)
+            {
+
+                Vector2 pushBackForceToAddAsVector = new Vector2(playerFacingDirection * pushBackForceOfFirstAttack, 0f);
+
+                enemyRigidBody.AddForce(pushBackForceToAddAsVector, ForceMode2D.Impulse);
+
+                Debug.Log("pushed");
+                if (!isGrounded)
+                {
+                    animator.Play("attack_JumpKick");
+                }
+                else
+                {
+                    animator.Play("attack_Kick");
+                }
+                doThePush = false;
+
+                enemyRigidBody = null; // to free up the rigidbody reference
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            Debug.Log("Collision with enemy successfully detected");
+
+            enemyRigidBody = collision.gameObject.GetComponent<Rigidbody2D>();
+
+        }
+    }
+
+    void CheckPlayerFacingDirection()
+    {
+        if (gameObject.transform.rotation.y == 0)
+        {
+            playerFacingDirection = 1; // 1 means right
+
+        }
+        else if (gameObject.transform.rotation.y == -1)
+        {
+            playerFacingDirection = -1;
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
     public IEnumerator AttackWhenAttackButtonIsPressed()
     {
-        if(pushBackScript.enemyRigidBody)
+        if(enemyRigidBody)
         {
-            statusSciptOfEnemy = pushBackScript.enemyRigidBody.gameObject.GetComponent<Statuses>();
-            //Debug.Log("Entered the main attack script");
+            statusSciptOfEnemy = enemyRigidBody.gameObject.GetComponent<Statuses>();
+            
 
 
             if (Input.GetKeyDown(KeyCode.Mouse0))
@@ -58,7 +164,7 @@ public class AttackMain : MonoBehaviour
                     playerControllerReference.canMove = false;
                     canAttack = false;
                     yield return new WaitForSeconds(windingUpTimeOfFirstAttack);
-                    pushBackScript.doThePush = true;
+                    doThePush = true;
                     Debug.Log("Attack button pressed part");
                     statusSciptOfEnemy.DecreaseHealthByTheNumber(damageOfFirstAttack);
 
