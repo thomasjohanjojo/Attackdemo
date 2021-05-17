c2c872d8620b69760c56982ebac0758e0c086558
Thomas Johan Jojo <thomasjohanjojo@gmail.com>
2021-05-17 19:46:57 +0530

    A platform with basic functionality has been achieved

--- Attackdemo/Assets/Scripts/MovingPlatformMainScript.cs
+++ Attackdemo/Assets/Scripts/MovingPlatformMainScript.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class MovingPlatformMainScript : MonoBehaviour
 {
+
+    public Rigidbody2D rigidbody2DOfThisPlatform;
+
+    public float lowerYAxisLimitOfPlatform;
+    public float velocityOfPlatform;
+    [SerializeField] private float upperYAxisLimitOfPlatform;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        upperYAxisLimitOfPlatform = transform.position.y;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.y >= upperYAxisLimitOfPlatform)
+        {
+            rigidbody2DOfThisPlatform.velocity = new Vector2(0f, -velocityOfPlatform);
+        }
+
+        else if(transform.position.y <= (upperYAxisLimitOfPlatform - lowerYAxisLimitOfPlatform))
+        {
+            rigidbody2DOfThisPlatform.velocity = new Vector2(0f, velocityOfPlatform);
+        }
     }
 }
